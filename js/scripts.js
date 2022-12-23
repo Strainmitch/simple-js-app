65cb9b1870dfcb6afd5bcf974b4a6567a3bc11fe
Mitchell Strain <116252008+Strainmitch@users.noreply.github.com>
2022-12-22 22:11:02 -0600

    pokedex update
    Added  the variable pokemonList to the js script with 3 objects and strings for each.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
-let favoriteFood = 'Mediterranean';
-document.write(favoriteFood);
+let pokemonList = [
+    { ID: "25", name: "Pikachu", height: 0.4, type: ["electric"] },
+    { ID: "150", name: "Mewtwo", height: 2 type: ["psychic"] },
+    { ID: "6", name: "Charizard" height: 1.7, type: ["fire","flying"] },
+];
+
