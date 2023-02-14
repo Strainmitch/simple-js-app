46af28975d1d5810ee9bb19ec5d6590af7411703
Mitchell Strain <116252008+Strainmitch@users.noreply.github.com>
2023-02-14 11:53:32 -0600

    changes done to my script and index

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,41 @@
 let pokemonRepository = (function () {
-let pokemonList = [
-    { ID: "6", name: "Charizard", height: 1.7, type: ["fire","flying"] },
-    { ID: "7", name: "Squirtle", height: .50, type: ["water"] },
-    { ID: "12", name: "Butterfree", height: 1.09, type: ["bug","flying"] },
-    { ID: "24", name: "Arbok", height: 3.5, type: ["poison"] },
-    { ID: "25", name: "Pikachu", height: .40, type: ["electric"] },
-    { ID: "30", name: "Nidorina", height: .78, type: ["poison"] },
-    { ID: "45", name: "Vileplume", height: 1.19, type: ["grass","poison"] },
-    { ID: "91", name: "Cloyster", height: 1.49, type: ["fire","flying"] },
-    { ID: "150", name: "Mewtwo", height: 2, type: ["psychic"] },
-    { ID: "395", name: "Empoleon", height: 1.70, type: ["water","steel"] }
+  let pokemonList = [
+    { name: "Charizard", height: 1.7, type: ["fire","flying"] },
+    { name: "Squirtle", height: .50, type: ["water"] },
+    { name: "Butterfree", height: 1.09, type: ["bug","flying"] },
+    { name: "Arbok", height: 3.5, type: ["poison"] },
+    { name: "Pikachu", height: .40, type: ["electric"] },
+    { name: "Nidorina", height: .78, type: ["poison"] },
+    { name: "Vileplume", height: 1.19, type: ["grass","poison"] },
+    { name: "Cloyster", height: 1.49, type: ["fire","flying"] },
+    { name: "Mewtwo", height: 2, type: ["psychic"] },
+    { name: "Empoleon", height: 1.70, type: ["water","steel"] }
 ]
 
 function add(pokemon) {
-    pokemonList.push(pokemon); 
-  } 
-  
-  function getAll() {
-    return pokemonList;
-  }
-  
-  return {
-    add: add,
-    getAll: getAll
-  };
-  })();
+  pokemonList.push(pokemon);
+}
+
+function getAll() {
+  return pokemonList;
+}
+
+return {
+  add: add,
+  getAll: getAll
+};
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write(pokemon.name +  pokemon.height);
+});
 
 pokemonList.forEach(function(pokemon) {
-    if (pokemon.height >=5) {
-      document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a big Pokemon!!" + "<br>")
-    } else if (pokemon.height >= 1.6 && pokemon.height < 5) {
-      document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a normal Pokemon!" + "<br>")
-    } else {
-      document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a small pokemon!" + "<br>")
-    }
-  });
-
-  pokemonRepository.getAll().forEach(function(pokemon){
-    document.write(pokemon.name + pokemon.height);
-  });
+  if (pokemon.height >=5) {
+    document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a big Pokemon!!" + "<br>")
+  } else if (pokemon.height >= 1.6 && pokemon.height < 5) {
+    document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a normal Pokemon!" + "<br>")
+  } else {
+    document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a small pokemon!" + "<br>")
+  }
+});
